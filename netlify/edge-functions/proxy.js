d1938c1c1c2df23d075635295be982f921051345
Jarrett Gliner <jarrett@amaze.co>
2023-03-08 15:03:03 -0700

    Add static redirects for everything except doc root, assume local proxy, assume bundle modified

--- netlify/edge-functions/proxy.js
+++ netlify/edge-functions/proxy.js
@@ -1,15 +1,17 @@
 export default async function handler(req, context) {
+  const urlObj = new URL(req.url);
   const storeId = Deno.env.get('STORE_ID');
-  const proxyBase = `${storeId}.creator-spring.com`;
-  const proxyTarget = `https://${proxyBase}${new URL(req.url).pathname}`;
+  const proxyTarget = 'http://localhost:3002'; // This will be the new generic bundle
 
   const creatorSpringReq = await fetch(proxyTarget);
   const creatorSpringStream = await creatorSpringReq.body;
   let creatorSpringContents = await new Response(creatorSpringStream).text();
 
-  creatorSpringContents = creatorSpringContents.replace(/window\.location\.hostname/gim, `'${proxyBase}'`)
+  const preSplice = creatorSpringContents.slice(0, creatorSpringContents.lastIndexOf('</head>'));
+  const postSplice = creatorSpringContents.slice(creatorSpringContents.lastIndexOf('</head>'));
+  creatorSpringContents = `${preSplice}<script>window.STORE_ID='${storeId}'</script>${postSplice}`
 
   return new Response(creatorSpringContents, { headers: Object.fromEntries(creatorSpringReq.headers.entries()) });
 }
 
-export const config = { path: '/*' }
+export const config = { path: '/' }
